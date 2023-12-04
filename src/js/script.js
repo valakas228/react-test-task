c4468868f8abc36baf9c8b00b59fb9021051b74f
valakas228 <55451937+valakas228@users.noreply.github.com>
2023-12-04 22:43:02 +0300

    deleted non-used vars

--- src/js/script.js
+++ src/js/script.js
@@ -1,54 +1,35 @@
-var IsMenuShow = false;
-var IsMenuShow2 = false;
-var IsMenuShow3 = false;
-var IsMenuShow4 = false;
-var IsMenuShow5 = false;
-var IsAboutMoreContentShown = false
-let offset = 0;
+const moreBtns = document.querySelectorAll(".more-btn");
 
-
-
-
-
-
-
-const moreBtns = document.querySelectorAll('.more-btn');
-
-moreBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
+moreBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
     const product = btn.parentNode;
-    const moreInfo = product.querySelector('.more-info');
+    const moreInfo = product.querySelector(".more-info");
 
-    moreInfo.classList.toggle('show');
-    if (moreInfo.classList.contains('show')) {
-      btn.textContent = 'Скрыть';
+    moreInfo.classList.toggle("show");
+    if (moreInfo.classList.contains("show")) {
+      btn.textContent = "Скрыть";
     } else {
-      btn.textContent = 'Подробнее';
+      btn.textContent = "Подробнее";
     }
   });
 });
 
-const btn = document.querySelector('.more-btn');
-
-
-
-const faqList = document.querySelector('.faq__list');
-const faqQuestions = faqList.querySelectorAll('.faq__question');
+const faqList = document.querySelector(".faq__list");
+const faqQuestions = faqList.querySelectorAll(".faq__question");
 
 faqQuestions.forEach((question) => {
-  question.addEventListener('click', () => {
-    question.classList.toggle('active');
+  question.addEventListener("click", () => {
+    question.classList.toggle("active");
     const answer = question.nextElementSibling;
     if (answer.style.maxHeight) {
       answer.style.maxHeight = null;
     } else {
-      answer.style.maxHeight = answer.scrollHeight + 'px';
+      answer.style.maxHeight = answer.scrollHeight + "px";
     }
   });
 });
 
-
-const animatedElements = document.querySelectorAll('.more-btn');
+const animatedElements = document.querySelectorAll(".more-btn");
 
 function checkScroll() {
   animatedElements.forEach((element) => {
@@ -56,11 +37,11 @@ function checkScroll() {
     const elementBottom = element.getBoundingClientRect().bottom;
 
     if (elementTop < window.innerHeight && elementBottom > 0) {
-      element.classList.add('animate');
+      element.classList.add("animate");
     } else {
-      element.classList.remove('animate');
+      element.classList.remove("animate");
     }
   });
 }
 
-window.addEventListener('scroll', checkScroll);
+window.addEventListener("scroll", checkScroll);
