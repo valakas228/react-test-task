fdd23fd75cf756d98b3f8db7b1ebe830e08368b3
valakas228 <55451937+valakas228@users.noreply.github.com>
2023-12-06 19:14:38 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import * as ReactDOMClient from 'react-dom/client';
-import './css/styles.css'
-import './css/media.css'
-import Header from './components/header'
-import Main from './components/main'
-import Footer from './components/footer'
-import './js/script'
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import "./styles/styles.css";
+import "./styles/media.css";
+import Header from "./components/header";
+import Main from "./components/main";
+import Footer from "./components/footer";
+import "./js/script";
 
-const main = ReactDOMClient.createRoot(document.getElementById('main'))
-const header = ReactDOMClient.createRoot(document.getElementById('header'))
-const footer = ReactDOMClient.createRoot(document.getElementById('footer'))
+const main = ReactDOMClient.createRoot(document.getElementById("main"));
+const header = ReactDOMClient.createRoot(document.getElementById("header"));
+const footer = ReactDOMClient.createRoot(document.getElementById("footer"));
 
-header.render(<Header />)
+header.render(<Header />);
 main.render(<Main />);
-footer.render(<Footer />)
+footer.render(<Footer />);
