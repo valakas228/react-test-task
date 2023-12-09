19f785b66b3785628ceb154c0698d595e7dbbd80
valakas228 <55451937+valakas228@users.noreply.github.com>
2023-12-10 02:53:58 +0400

    Update store.jsx

---
+++ src/components/store.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Store = () => {
+  return (
+    <>
+      <div className="content-main-block-background"></div>
+      <div className="products">
+        <div className="product-grid">
+          <div className="product">
+            <div className="card-body">
+              <h5 className="card-title"></h5>
+              <p className="card-text"></p>
+            </div>
+            <div className="card-price">
+              <a className="button-shop"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Store;
